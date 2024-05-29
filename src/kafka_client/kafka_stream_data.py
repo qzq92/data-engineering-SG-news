c4da2e8126323849a5d06b4a2847f3470fbeafcb
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-05-29 17:09:08 +0800

    Added draft modifications

--- src/kafka_client/kafka_stream_data.py
+++ src/kafka_client/kafka_stream_data.py
@@ -1,24 +1,23 @@
 from src.constants import (
-    URL_API,
+    URL,
+    URL_TOPIC,
     PATH_LAST_PROCESSED,
-    MAX_LIMIT,
-    MAX_OFFSET,
+    KAFKA_PRODUCER_DEFAULT_PORT,
+    KAFKA_PRODUCER_LOCAL_PORT
 )
-
 from .transformations import transform_row
-
+from kafka import KafkaProducer
+from typing import List
+import feedparser
 import kafka.errors
 import json
 import datetime
-import requests
-from kafka import KafkaProducer
-from typing import List
 import logging
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO, force=True)
 
 
-def get_latest_timestamp():
+def get_latest_timestamp_from_file():
     """
     Gets the latest timestamp from the last_processed.json file
     """
@@ -41,50 +40,46 @@ def update_last_processed_file(data: List[dict]):
     ]
     last_processed = max(publication_dates_as_timestamps) - datetime.timedelta(days=1)
     last_processed_as_string = last_processed.strftime("%Y-%m-%d")
+    # Overwrite data
     with open(PATH_LAST_PROCESSED, "w") as file:
         json.dump({"last_processed": last_processed_as_string}, file)
 
 
-def get_all_data(last_processed_timestamp: datetime.datetime) -> List[dict]:
-    n_results = 0
-    full_data = []
-    while True:
-        # The publication date must be greater than the last processed timestamp and the offset (n_results)
-        # corresponds to the number of results already processed.
-        url = URL_API.format(last_processed_timestamp, n_results)
-        response = requests.get(url)
-        data = response.json()
-        current_results = data["results"]
-        full_data.extend(current_results)
-        n_results += len(current_results)
-        if len(current_results) < MAX_LIMIT:
-            break
-        # The sum of offset + limit API parameter must be lower than 10000.
-        if n_results + MAX_LIMIT >= MAX_OFFSET:
-            # If it is the case, change the last_processed_timestamp parameter to the date_de_publication
-            # of the last retrieved result, minus one day. In case of duplicates, they will be filtered
-            # in the deduplicate_data function. We also reset n_results (or the offset parameter) to 0.
-            last_timestamp = current_results[-1]["date_de_publication"]
-            timestamp_as_date = datetime.datetime.strptime(last_timestamp, "%Y-%m-%d")
-            timestamp_as_date = timestamp_as_date - datetime.timedelta(days=1)
-            last_processed_timestamp = timestamp_as_date.strftime("%Y-%m-%d")
-            n_results = 0
+def get_all_data(last_processed_timestamp: datetime.datetime, url: str) -> List[dict]:
+    """Get all data from configured URL and update processed timestamp info when data is available.
 
-    logging.info(f"Got {len(full_data)} results from the API")
+    Args:
+        last_processed_timestamp (datetime.datetime): _description_
 
-    return full_data
+    Returns:
+        List[dict]: _description_
+    """
+    full_data_list = []
+    data = feedparser.parse(url)
+    current_articles_list = data["articles"]
+    full_data_list.extend(current_articles_list)
+    
+    if len(full_data_list):
+        # If it is the case, change the last_processed_timestamp parameter to the date of publication minus one day. In case of duplicates, they will be filtered
+        last_timestamp = current_articles_list[0]["published"]
+        last_processed_timestamp = datetime.datetime.strptime(last_timestamp, "%a, %d %b %Y %H:%M:%S %z")
+
+
+    logging.info(f"Got {len(full_data_list)} results from the API")
+
+    return full_data_list, last_processed_timestamp
 
 
 def deduplicate_data(data: List[dict]) -> List[dict]:
-    return list({v["reference_fiche"]: v for v in data}.values())
+    return list({v["id"]: v for v in data}.values())
 
 
 def query_data() -> List[dict]:
     """
-    Queries the data from the API
+    Queries the data from the API of interest. Called under __main__
     """
-    last_processed = get_latest_timestamp()
-    full_data = get_all_data(last_processed)
+    last_processed_date = get_latest_timestamp_from_file()
+    full_data = get_all_data(last_processed_timestamp=last_processed_date, url=URL)
     full_data = deduplicate_data(full_data)
     if full_data:
         update_last_processed_file(full_data)
@@ -98,32 +93,52 @@ def process_data(row):
     return transform_row(row)
 
 
-def create_kafka_producer():
+def create_kafka_producer(cluster_port: int, ext_port: int):
+    """Creates the Kafka producer object (broker) in the cluster using specified default port within a cluster and enable external connection to cluster via fallback_port
+
+    Args:
+        default_port (int, optional): _description_.
+        fallback_port (int, optional): _description_.
+
+    Returns:
+        _type_: _description_
     """
-    Creates the Kafka producer object
-    """
+    cluster_port = str(cluster_port)
+    ext_port = str(ext_port)
     try:
-        producer = KafkaProducer(bootstrap_servers=["kafka:9092"])
+        # A broker that facilitates transactions between consumers and producers
+        producer = KafkaProducer(bootstrap_servers=[f"kafka:{cluster_port}"])
     except kafka.errors.NoBrokersAvailable:
         logging.info(
-            "We assume that we are running locally, so we use localhost instead of kafka and the external "
-            "port 9094"
+            f"No brokers available, so we use localhost instead of kafka via external port {ext_port}"
         )
-        producer = KafkaProducer(bootstrap_servers=["localhost:9094"])
+        # External comms
+        producer = KafkaProducer(bootstrap_servers=[f"localhost:{ext_port}"])
 
     return producer
 
+# Callbacks for success delivery
+def on_send_success():
+    logging.info("Deliveries sent successfully")
+
+# Callbacks for
+def on_send_error(excp):
+    logging.error('Deliveries encountered error', exc_info=excp)
 
 def stream():
     """
-    Writes the API data to Kafka topic rappel_conso
+    Writes the API data to Kafka topic 'CNA_rss_business' upon transformation
     """
-    producer = create_kafka_producer()
+    producer = create_kafka_producer(cluster_port=9092, ext_port=9094)
     results = query_data()
+    # Transform data after querying and send for consumption by looping through
     kafka_data_full = map(process_data, results)
+
+    #publish data
     for kafka_data in kafka_data_full:
-        producer.send("rappel_conso", json.dumps(kafka_data).encode("utf-8"))
+        producer.send(URL_TOPIC, json.dumps(kafka_data).encode("utf-8")).add_callback(on_send_success).add_errback(on_send_error)
 
-
+    producer.flush()
+# Main function
 if __name__ == "__main__":
     stream()
--- src/kafka_client/transformations.py
+++ src/kafka_client/transformations.py
@@ -1,54 +1,38 @@
-import re
 from unidecode import unidecode
-from src.constants import COLUMNS_TO_NORMALIZE, COLUMNS_TO_KEEP
+from src.constants import COLUMNS_TO_NORMALIZE, COLUMNS_TO_KEEP, COLUMNS_TO_EXPAND
 
+def flatten_dict(data_dict, separator='_', prefix=''):
+    res = {}
+    for key, value in data_dict.items():
+        if isinstance(value, dict):
+            res.update(flatten_dict(value, separator, prefix + key + separator))
+        else:
+            res[prefix + separator + key] = value
+    return res
 
-def merge_two_columns(
-    col_a: str, col_b: str, row: dict, normalize: bool = True
-) -> dict:
-    val_col_a = row.get(col_a)
-    val_col_b = row.get(col_b)
-    new_col = ""
-    next_start_char = ""
-    if val_col_a:
-        new_col = val_col_a
-        next_start_char = "\n"
-    if val_col_b:
-        new_col += next_start_char + val_col_b
-    final = new_col or None
-    if final and normalize:
-        final = normalize_one(final)
-    return final
-
-
-def separate_commercialisation_dates(row: dict) -> tuple:
-    date_debut_fin = row.get("date_debut_fin_de_commercialisation")
-    if not date_debut_fin:
-        return None, None
-
-    date_debut_commercialisation = None
-    date_fin_commercialisation = None
-    date_pattern = r"(\d{2}/\d{2}/\d{4})"
-    patterns = re.findall(date_pattern, date_debut_fin)
-    if len(patterns) == 2:
-        date_debut_commercialisation = patterns[0]
-        date_fin_commercialisation = patterns[1]
-    elif len(patterns) == 1:
-        if "depuis le" in date_debut_fin.lower():
-            date_debut_commercialisation = patterns[0]
-        elif "jusqu" in date_debut_fin.lower():
-            date_fin_commercialisation = patterns[0]
-    return date_debut_commercialisation, date_fin_commercialisation
+def expand_nested_columns(api_row: dict) -> tuple:
+    for col in COLUMNS_TO_EXPAND:
+        api_row.update(flatten_dict(data_dict=api_row[col], prefix=col))
+    
+    return api_row
 
 
 def normalize_one(text: str) -> str:
     """
-    Remove accents.
+    Checks if string is chinese hanzi and converts to pinyin. Subsequently remove accents for alphabets 
     """
     return unidecode(text)
 
 
 def normalize_columns(api_row: dict) -> dict:
+    """Function which applies normalisation on columns listed in COLUMNS_TO_NORMALIZE config params. 
+
+    Args:
+        api_row (dict): _description_
+
+    Returns:
+        dict: _description_
+    """
     kafka_row = {}
     for col in COLUMNS_TO_KEEP:
         kafka_row[col] = api_row.get(col)
@@ -60,26 +44,18 @@ def normalize_columns(api_row: dict) -> dict:
 
     return kafka_row
 
-
+# Key transformation work post data pull
 def transform_row(api_row: dict) -> dict:
+    """Function which executes necessary transformation for kafka retrieved data
+
+    Args:
+        api_row (dict): _description_
+
+    Returns:
+        dict: _description_
+    """
     kafka_row = normalize_columns(api_row)
 
-    kafka_row["risques_pour_le_consommateur"] = merge_two_columns(
-        "risques_encourus_par_le_consommateur",
-        "description_complementaire_du_risque",
-        api_row,
-    )
-    kafka_row["recommandations_sante"] = merge_two_columns(
-        "preconisations_sanitaires",
-        "conduites_a_tenir_par_le_consommateur",
-        api_row,
-    )
-    kafka_row["informations_complementaires"] = merge_two_columns(
-        "informations_complementaires",
-        "informations_complementaires_publiques",
-        api_row,
-    )
-    sep_columns = separate_commercialisation_dates(api_row)
-    kafka_row["date_debut_commercialisation"] = sep_columns[0]
-    kafka_row["date_fin_commercialisation"] = sep_columns[1]
+    kafka_row = expand_nested_columns(api_row)
+
     return kafka_row
