c34091463bd9223351a2fda4c674ba23fc07b296
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-07 11:14:33 +0800

    Added spark pkg and postgres config

--- src/constants.py
+++ src/constants.py
@@ -2,9 +2,8 @@ import os
 
 # API params
 PATH_LAST_PROCESSED = "./data/last_processed.json"
-KAFKA_PRODUCER_CLUSTER_PORT = 9092 # Any changes must 
+KAFKA_PRODUCER_CLUSTER_PORT = 9092
 KAFKA_PRODUCER_EXT_PORT = 9094
-
 URL = f"https://www.channelnewsasia.com/api/v1/rss-outbound-feed?_format=xml&category=6936"
 URL_TOPIC = "CNA_Business"
 
@@ -12,12 +11,12 @@ URL_TOPIC = "CNA_Business"
 # POSTGRES PARAMS. Defaults to localhost if POSTGRES_DOCKER_USER exist
 user_name = os.getenv("POSTGRES_DOCKER_USER", "localhost")
 POSTGRES_URL = f"jdbc:postgresql://{user_name}:5432/postgres"
-POSTGRES_PROPERTIES = {
-    "user": "postgres",
-    "password": os.getenv("POSTGRES_PASSWORD"),
-    "driver": "org.postgresql.Driver",
-}
+POSTGRES_PASSWORD =  "admin"
+POSTGRES_USER = "admin"
+POSTGRES_DB = "admin"
 POSTGRES_TABLE_NAME = URL_TOPIC
+
+SPARK_PKG = "org.postgresql:postgresql:42.7.3,org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4"
 
 # Columns to expand
 COLUMNS_TO_EXPAND= []
