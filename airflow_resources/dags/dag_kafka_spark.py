e2acfe838cfa99ba8565fa380ce9f4ba197096a1
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-07 11:16:18 +0800

    Added comments

--- airflow_resources/dags/dag_kafka_spark.py
+++ airflow_resources/dags/dag_kafka_spark.py
@@ -3,13 +3,12 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
-
 from src.kafka_client.kafka_stream_data import stream
-
-
+from src.constants import URL_TOPIC, SPARK_PKG
+# Define start day as 1 day before date of execution
 start_date = datetime.today() - timedelta(days=1)
 
-
+# Define arguments for DAG: Concerned with how to execute them
 default_args = {
     "owner": "airflow",
     "start_date": start_date,
@@ -17,31 +16,35 @@ default_args = {
     "retry_delay": timedelta(seconds=5),
 }
 
-
+# Define DAG as context manager
 with DAG(
     dag_id="kafka_spark_dag",
-    default_args=default_args,
-    schedule_interval=timedelta(days=1),
+    default_args=default_args, # Contains other key value pairs
+    schedule_interval=timedelta(days=1), #Alternative can be schedule="@daily"
     catchup=False,
 ) as dag:
 
+    # Define DAG tasks
+    # Python task
     kafka_stream_task = PythonOperator(
         task_id="kafka_data_stream",
-        python_callable=stream,
+        python_callable=stream, # python function
+        op_kwargs = None,
         dag=dag,
     )
 
+    # Docker task
     spark_stream_task = DockerOperator(
         task_id="pyspark_consumer",
-        image="rappel-conso/spark:latest",
+        image=f"{URL_TOPIC}/spark:latest", # Our docker image to run
         api_version="auto",
-        auto_remove=True,
-        command="./bin/spark-submit --master local[*] --packages org.postgresql:postgresql:42.5.4,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 ./spark_streaming.py",
+        auto_remove=True, # Remove after completion
+        command=f"./bin/spark-submit --master local[*] --packages {SPARK_PKG} ./spark_streaming.py",
         docker_url='tcp://docker-proxy:2375',
-        environment={'SPARK_LOCAL_HOSTNAME': 'localhost'},
+        environment={'SPARK_LOCAL_HOSTNAME': 'localhost'}, # Docker image environment
         network_mode="airflow-kafka",
         dag=dag,
     )
 
-
+    # Dependency order
     kafka_stream_task >> spark_stream_task
