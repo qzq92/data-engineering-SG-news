090a805cb59532cbf0382c22ec572b682ab6692b
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-07 11:37:18 +0800

    Added comments to statements in code

--- scripts/create_table.py
+++ scripts/create_table.py
@@ -10,32 +10,45 @@ conn = psycopg2.connect(dbname=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRE
 cur = conn.cursor()
 
 
-def try_execute_sql(sql: str):
+def try_execute_sql(sql: str) -> None:
+    """Function which takes in a sql string for execution.
+
+    Args:
+        sql (str): Input SQL string.
+    """
     try:
         cur.execute(sql)
+        #Commit any pending transactions to the database.
         conn.commit()
         print(f"Executed table creation successfully")
     except Exception as e:
         print(f"Couldn't execute table creation due to exception: {e}")
+        # Good practice to rollback after to ensure proper reset.
         conn.rollback()
 
 
-def create_table():
+def create_table() -> None:
+    """Creates the CNA table and its columns, witb the first field serving as PRIMARY KEY.
     """
-    Creates the CNA table and its columns.
-    """
+
+    # Create table sql command string
     create_table_sql = f"""
     CREATE TABLE CNA (
         {DB_FIELDS[0]} text PRIMARY KEY,
     """
+
+    # Extend sql query statement for other fields with text type
     for field in DB_FIELDS[1:-1]:
         column_sql = f"{field} text, \n"
         create_table_sql += column_sql
 
+    # Terminating string  
     create_table_sql += f"{DB_FIELDS[-1]} text \n" + ");"
     try_execute_sql(create_table_sql)
 
+    # Close pointer
     cur.close()
+    # Close connection to DB
     conn.close()
 
 
